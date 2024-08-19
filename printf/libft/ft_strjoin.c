3315782ed9efd0b6acb4e27a341d72e59706ccde
Ali Al Mokdad <aal-mokd@c1r1s1.42beirut.com>
2024-08-19 14:05:51 +0300

    19-8-2024

---
+++ printf/libft/ft_strjoin.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aal-mokd <aal-mokd@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/18 13:11:47 by aal-mokd          #+#    #+#             */
+/*   Updated: 2024/06/18 13:26:17 by aal-mokd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		l1;
+	int		l2;
+	char	*sum;
+	char	*start;
+
+	l1 = ft_strlen(s1);
+	l2 = ft_strlen(s2);
+	sum = (char *) malloc(sizeof(char) * (l1 + l2 +1));
+	if (sum == NULL)
+		return (NULL);
+	start = sum;
+	while (l1--)
+		*sum++ = *s1++;
+	while (l2--)
+		*sum++ = *s2++;
+	*sum = '\0';
+	return (start);
+}
